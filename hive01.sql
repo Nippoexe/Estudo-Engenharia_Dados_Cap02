5b64d71f8d9ba3804aa60ee20684ae666bcaa8f7
Leandro Seiji Yoshimatsu <54373082+Nippoexe@users.noreply.github.com>
2025-02-14 19:37:38 -0300

    Add files via upload

---
+++ hive01.sql
@@ -0,0 +1,13 @@
+create table estudantes
+(ID INT,
+nome VARCHAR(50),
+sobrenome VARCHAR(50),
+sexo Char(1),
+email VARCHAR(100));
+
+
+INSERT INTO estudantes
+VALUES (100,'Bob','Silva','M','bob@gmail.com');
+
+SELECT * FROM estudantes;
+
